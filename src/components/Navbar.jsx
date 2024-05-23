764319339828e7f98781b4b79dbefa4efe51ec33
anaualima <alissandraraysa@gmail.com>
2024-05-23 11:11:26 -0300

    create concessions tolltip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import { BiSearchAlt2 } from 'react-icons/bi';
 import { LuPopcorn } from "react-icons/lu";
 
 import filmaxLogo from '../images/filmaxLogo.png';
+import Tippy from '@tippyjs/react';
+
 import './Navbar.css';
+import 'tippy.js/dist/tippy.css'
 
 const Navbar = () => {
   const [search, setSearch] = useState("");
@@ -12,28 +15,30 @@ const Navbar = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!search) return
+    if (!search) return
     navigate(`/search?q=${search}`)
     setSearch("");
   }
   return (
     <nav id="navbar">
       <div>
-        <Link to="concessions"><LuPopcorn /></Link>
+        <Tippy content="Concessions">
+          <Link to="concessions"><LuPopcorn /></Link>
+        </Tippy>
       </div>
       <h2>
         <Link to="/">
-        <img src={filmaxLogo} alt="filmax-logo"/>
+          <img src={filmaxLogo} alt="filmax-logo" />
         </Link>
       </h2>
       <form onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        placeholder="Search a movie"
-        onChange={(e) => setSearch(e.target.value)}
-        value={search}
+        <input
+          type="text"
+          placeholder="Search a movie"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
         />
-        <button type="submit"><BiSearchAlt2/></button>
+        <button type="submit"><BiSearchAlt2 /></button>
       </form>
     </nav>
   )
